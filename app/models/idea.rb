66145bf4ee554b37d5309cb4666b5a7038852ae6
Murugan <murugu2012@gmail.com>
2017-01-02 20:17:16 +0530

    Validate presence of ideas.price_category

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -5,6 +5,8 @@ class Idea < ApplicationRecord
 
   # Validations
 
+  validates :price_category, :presence => true
+
   validates :price_category, :inclusion => { :in => [ 'Inexpensive', 'Mid-Range', 'Pricy', 'Luxurious' ]  }
 
   validates :user_id, :uniqueness => { :scope => [:date_title] }
