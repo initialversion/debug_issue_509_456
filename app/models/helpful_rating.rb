4a6954b84a5302536edddb9fb7c8ff5a968baf3b
Murugan <murugu2012@gmail.com>
2017-01-02 20:17:17 +0530

    Direct association between reviews and helpful_ratings

--- app/models/helpful_rating.rb
+++ app/models/helpful_rating.rb
@@ -1,6 +1,8 @@
 class HelpfulRating < ApplicationRecord
   # Direct associations
 
+  belongs_to :review
+
   # Indirect associations
 
   # Validations
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  has_many   :helpful_ratings,
+             :dependent => :destroy
+
   has_many   :photos,
              :dependent => :destroy
 
