1deba2f74d16d17e156ce4218edb0bdc20bc0fde
Murugan <murugu2012@gmail.com>
2017-01-02 20:17:16 +0530

    Validate uniqueness of reviews.date_idea_rating_comment

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,8 @@ class Review < ApplicationRecord
 
   # Validations
 
+  validates :date_idea_rating_comment, :uniqueness => { :scope => [:date_idea_id, :user_id] }
+
   validates :user_id, :uniqueness => { :scope => [:date_idea_id] }
 
   validates :user_id, :presence => true
