bbde1d0609aa1593354356cdd1053ea205f80d2f
Murugan <murugu2012@gmail.com>
2017-01-02 20:17:41 +0530

    Whitelist columns in Review for ActiveAdmin

--- app/admin/review.rb
+++ app/admin/review.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Review do
 
+ permit_params :date_idea_id, :user_id, :date_idea_rating, :date_idea_rating_comment
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
