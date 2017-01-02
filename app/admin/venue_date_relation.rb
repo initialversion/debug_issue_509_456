eee1524dac9f31b8ff390a04337410959b286f8a
Murugan <murugu2012@gmail.com>
2017-01-02 20:17:49 +0530

    Whitelist columns in VenueDateRelation for ActiveAdmin

--- app/admin/venue_date_relation.rb
+++ app/admin/venue_date_relation.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register VenueDateRelation do
 
+ permit_params :date_id, :venue_id, :information
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
