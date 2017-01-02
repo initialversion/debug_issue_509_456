057bf4f682f2ed68298ac6414f5dc882d5a201db
Murugan <murugu2012@gmail.com>
2017-01-02 20:17:16 +0530

    Validate presence of venue_date_relations.information

--- app/models/venue_date_relation.rb
+++ app/models/venue_date_relation.rb
@@ -5,6 +5,8 @@ class VenueDateRelation < ApplicationRecord
 
   # Validations
 
+  validates :information, :presence => true
+
   validates :venue_id, :uniqueness => { :scope => [:date_id] }
 
   validates :venue_id, :presence => true
