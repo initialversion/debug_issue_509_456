fd9e56c82990e23511926fa15650d8e50dee93b5
Murugan <murugu2012@gmail.com>
2017-01-02 20:17:16 +0530

    Validate presence of photos.photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,8 @@ class Photo < ApplicationRecord
 
   # Validations
 
+  validates :photo, :presence => true
+
   validates :photo_description, :presence => true
 
   validates :review_id, :uniqueness => { :scope => [:photo] }
